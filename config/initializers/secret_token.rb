7ae865b36d7659fda630b00a2508ee9dcd053865
Ajay Singh <meajaysingh@hotmail.com>
2014-03-16 22:39:54 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sharethat::Application.config.secret_key_base = '356af2ff7dc5cde67248de404ac32491f51235d3fbde1cc14c6a43dbca2469f817ce15cd271fb4a2452473ad8773d064e4ab0142d0fdd80272e8e93bc76acbac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sharethat::Application.config.session_store :cookie_store, key: '_sharethat_session'
