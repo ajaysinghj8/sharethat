925fbd035e2b88f93d86dbf3c4aeedc7a177ab3e
Ajay Singh <meajaysingh@hotmail.com>
2014-03-17 14:37:21 +0530

    view MODIFIED2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,14 @@ class HomeController < ApplicationController
  
 layout 'main_app'
 
-  def index
-  	@limit = 4
+   def initilize	
+     @category = BlogCategory.all
+   end
+   def index
+   	@limit = 4
   	@offset = 0
     @posts_all = BlogPost.newest_first @limit,@offset
-  end
+   end
  
    def single
      render('index')
