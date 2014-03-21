086f54b74f6c615f826586435c47e7eb0af0c43b
Ajay Singh <meajaysingh@hotmail.com>
2014-03-21 15:46:28 +0530

    single page categories working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,6 +13,13 @@ layout 'main_app'
    end
  
    def single
-     render('index')
+    @category = BlogCategory.all
+    @popular_posts = BlogPost.popular_posts 3
+    @latest_comment = BlogComment.newest_first 3
+    @latest_users = BlogUser.newest_first 6
+    @limit = 4
+    @offset = 0
+    @posts_all = BlogPost.newest_first @limit,@offset
+  
    end
 end
